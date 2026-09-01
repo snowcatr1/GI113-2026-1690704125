7a2d9e6d775708fe2f2b8c718504740412693422
unknown <snowcatr1@gmail.com>
2026-09-01 15:47:06 +0700

    REAL Lab02 Part2

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -41,6 +41,61 @@ namespace Lab02
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
 
+            //Lab02 Part B
+            string sphynxName = "Luna";
+            char sphynxhealthyRank = 'A';
+            int sphynxHungry = 20;
+            double sphynxSleepy = 50;
+            bool sphynxHappy = true;
+
+            string persianName = "Pearl";
+            char persianhealthyRank = 'A';
+            int persianHungry = 23;
+            float persianSpeed = 8.2f;
+            bool persianHappy = true;
+
+            string mainecoonName = "Ruby";
+            char mainecoonhealthyRank = 'A';
+            int mainecoonHungry = 20;
+            float mainecoonSpeed = 7.5f;
+            bool mainecoonHappy = true;
+
+            string britishshorthairName = "Lucky";
+            char britishshorthairhealthyRank = 'A';
+            int britishshorthairHungry = 20;
+            float britishshorthairSpeed = 7.4f;
+            bool britishshorthairHappy = true;
+
+            Console.WriteLine("===== CAT LIST =====");
+
+            Console.WriteLine("----- SPHYNX -----");
+            Console.WriteLine($"Name: {sphynxName}");
+            Console.WriteLine($"Healthy: {sphynxhealthyRank}");
+            Console.WriteLine($"Hungry: {sphynxHungry}");
+            Console.WriteLine($"Sleepy: {sphynxSleepy}");
+            Console.WriteLine($"Is Happy: {sphynxHappy}");
+
+            Console.WriteLine("----- PERSIAN -----");
+            Console.WriteLine($"Name: {persianName}");
+            Console.WriteLine($"Healthy: {persianhealthyRank}");
+            Console.WriteLine($"Hungry: {persianHungry}");
+            Console.WriteLine($"Speed: {persianSpeed}");
+            Console.WriteLine($"Is Happy: {persianHappy}");
+
+            Console.WriteLine("----- MAINE COON -----");
+            Console.WriteLine($"Name: {mainecoonName}");
+            Console.WriteLine($"Healthy: {mainecoonhealthyRank}");
+            Console.WriteLine($"Hungry: {mainecoonHungry}");
+            Console.WriteLine($"Speed: {mainecoonSpeed}");
+            Console.WriteLine($"Is Happy: {mainecoonHappy}");
+
+            Console.WriteLine("----- BRITISH SHORTHAIR -----");
+            Console.WriteLine($"Name: {britishshorthairName}");
+            Console.WriteLine($"Healthy: {britishshorthairhealthyRank}");
+            Console.WriteLine($"Hungry: {britishshorthairHungry}");
+            Console.WriteLine($"Speed: {britishshorthairSpeed}");
+            Console.WriteLine($"Is Happy: {britishshorthairHappy}");
+
 
         }
     }
