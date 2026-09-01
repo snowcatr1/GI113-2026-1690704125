7d5e62c292f1083c601c02d671cb94c39eb7acc4
unknown <snowcatr1@gmail.com>
2026-09-01 16:39:21 +0700

    Lab03 Implicit Conversion: HP as double

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -12,7 +12,38 @@ namespace Lab03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+            var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level {level} / {MaxLevel}" +
+                $"\nHP: {currentHp} / {maxHp}" +
+                $"\nAttack Power: {attackPower}" +
+                $"\nCrit Multipiler: {critMultiplier}" +
+                $"\nIs Boss: {isBoss}");
+
+            // Implicit Conversation: currentHP (int) -->> double
+            Console.WriteLine("\n----- Implicit Conversion: HP as double -----");
+            double currentHpDouble = currentHp; //(double)currentHpDouble = (int)currentHp
+            Console.WriteLine($"HP (double): {currentHpDouble}");
+
+
+            // Explicit Conversion Cast attackPower (float) -->> int
+            Console.WriteLine("\n----- Exact HP Percent (no integer truncation) -----");
+            
+
+            // Cast vs. Convert critMultiplier (double) -->> int
+
         }
     }
 }
