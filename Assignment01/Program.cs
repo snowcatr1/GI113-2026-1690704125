2d2e571b6c50ebd58608d647e45ba3a83eb8a9e5
snowcatr1 <snowcatr1@gmail.com>
2026-09-11 20:33:23 +0700

    add Role and Skills
    Add Elliot Role = Healer
    Add Elliot Skills = Pizza Throw and Rush Hour

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -18,7 +18,8 @@ namespace Assignment01
             const int MaxLevel = 100;
 
             var survivorName = "Elliot";
-            var survivorRank = "A";
+            var survivorRole = "Healer";
+            char survivorRank = 'A';
             int survivorLevel = 1;
             int survivormaxHp = 80;
             int regularSpeed = 12;
@@ -27,8 +28,9 @@ namespace Assignment01
             bool isPlayable = true;
 
             Console.WriteLine($"=====+ {GameTitle} +=====");
-            Console.WriteLine($"===== {Survivor} =====");
+            Console.WriteLine($"\n===== {Survivor} =====");
             Console.WriteLine($"Name            : {survivorName}");
+            Console.WriteLine($"Role            : {survivorRole}");
             Console.WriteLine($"Rank            : {survivorRank}");
             Console.WriteLine($"Level           : {survivorLevel} / {MaxLevel}");
             Console.WriteLine($"Health          : {survivormaxHp}");
@@ -39,7 +41,7 @@ namespace Assignment01
 
             // Implicit Conversion
             double hpAsDouble = survivormaxHp;
-            Console.WriteLine($"\nHP as double (implicit) : {hpAsDouble}");
+            Console.WriteLine($"\nHP as double (implicit)  : {hpAsDouble}");
 
 
             // Explicit Cast
@@ -50,6 +52,12 @@ namespace Assignment01
 
             Console.WriteLine($"Stamina cast (truncates) : {staminaCast}");
             Console.WriteLine($"Stamina Convert (rounds) : {staminaConverted}");
+
+            const string ElliotSkills = "Skills";
+
+            Console.WriteLine($"\n=====+ {ElliotSkills} +=====");
+            Console.WriteLine("Pizza Throw : Heal other survivors");
+            Console.WriteLine("Rush Hour   : Gain Speed");
 
         }
     }
